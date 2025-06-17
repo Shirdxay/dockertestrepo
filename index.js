024194f5983e332d2197839ec0d82b1b20fccbaa
Shirdxay <c.shirdxay@gmail.com>
2025-06-17 10:00:54 +0700

    cdci added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi, CI/CD with Docker and Render!");
+  res.send("Gday, CI/CD with Docker and Render!");
 });
 
 const PORT = process.env.PORT || 3000;
